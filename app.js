b668194db54797b26ed1825b64088f204f765654
Derrick Strong <derrickmstrong@gmail.com>
2020-05-11 11:20:45 -0500

    Adding comments of additional ways to accomplish various steps

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let name = 'Derrick'; // Variable "name" is assigned the value of "Derrick"
+// Variable "name" is assigned the value of "Derrick"
+let name = 'Derrick'; 
+
 const numStates = 50;
 let sum = 5 + 4;
 
@@ -11,10 +13,17 @@ checkAge('John', 17);
 
 let favVeggies = ['carrots', 'cabbage', 'spinach', 'green peppers'];
 
-for (let i = 0; i < favVeggies.length; i++) {
-  const result = favVeggies[i];
-  console.log(result);
+// v1: Log array using For of loop
+for (const veggies of favVeggies) {
+  console.log(veggies);
 }
+
+// v2: Log array using For loop
+// for (let i = 0; i < favVeggies.length; i++) {
+//   const result = favVeggies[i];
+//   console.log(result);
+// }
+
 
 const pet = {
   name: 'GigaBite',
@@ -45,11 +54,17 @@ const visitors = [
   },
 ];
 
-for (let i = 0; i < visitors.length; i++) {
-  checkAge(visitors[i].name, visitors[i].age);
+// v1: Use the checkAge function to increment through array using the For of loop
+for (const visitor of visitors) {
+  checkAge(visitor.name, visitor.age)
 }
 
-/* Purely for testing purposes
+// v2: Use the checkAge function to increment through array using the For loop
+// for (let i = 0; i < visitors.length; i++) {
+//   checkAge(visitors[i].name, visitors[i].age);
+// }
+
+/* v3: For in loop - Purely for testing purposes
 
 for (const visitor in visitors) {
     if (visitors.hasOwnProperty(visitor)) {
@@ -60,15 +75,22 @@ for (const visitor in visitors) {
 
 */
 
-const results = getLength('Hello World');
-if (results % 2 == 0) {
-  console.log('The world is nice and even!');
-} else {
-  console.log('The world is an odd place!');
-}
+const length = getLength('Hello World');
+
+// v1: Check for even or odd using ternary operator
+getLength('Hello World') % 2 == 0
+  ? console.log('The world is nice and even!')
+  : console.log('The world is an odd place!');
+
+// v2: Check for even or odd using If statement
+// if (length % 2 == 0) {
+//   console.log('The world is nice and even!');
+// } else {
+//   console.log('The world is an odd place!');
+// }
+
 
 // Function Declarations
-
 function sayHello() {
   alert('Hello World!');
 }
@@ -76,6 +98,8 @@ function sayHello() {
 function checkAge(name, age) {
   if (age < 21) {
     alert('Sorry ' + name + ", you aren't old enough to view this page!");
+    // Using template literal
+    // alert(`Sorry ${name}, you aren't old enough to view this page!`); 
   }
 }
 
