0cc2be0bb72db0f8f21fbc1b4443a4edc6d1b32f
Derrick Strong <derrickmstrong@gmail.com>
2020-05-11 11:42:19 -0500

    Clean version

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variable "name" is assigned the value of "Derrick"
-let name = 'Derrick'; 
+let name = 'Derrick';
 
 const numStates = 50;
 let sum = 5 + 4;
@@ -13,17 +13,9 @@ checkAge('John', 17);
 
 let favVeggies = ['carrots', 'cabbage', 'spinach', 'green peppers'];
 
-// v1: Log array using For of loop
 for (const veggies of favVeggies) {
   console.log(veggies);
 }
-
-// v2: Log array using For loop
-// for (let i = 0; i < favVeggies.length; i++) {
-//   const result = favVeggies[i];
-//   console.log(result);
-// }
-
 
 const pet = {
   name: 'GigaBite',
@@ -54,41 +46,15 @@ const visitors = [
   },
 ];
 
-// v1: Use the checkAge function to increment through array using the For of loop
 for (const visitor of visitors) {
-  checkAge(visitor.name, visitor.age)
+  checkAge(visitor.name, visitor.age);
 }
-
-// v2: Use the checkAge function to increment through array using the For loop
-// for (let i = 0; i < visitors.length; i++) {
-//   checkAge(visitors[i].name, visitors[i].age);
-// }
-
-/* v3: For in loop - Purely for testing purposes
-
-for (const visitor in visitors) {
-    if (visitors.hasOwnProperty(visitor)) {
-        const name = visitors[visitor].name;
-        console.log(name);
-    }
-}
-
-*/
 
 const length = getLength('Hello World');
 
-// v1: Check for even or odd using ternary operator
 getLength('Hello World') % 2 == 0
   ? console.log('The world is nice and even!')
   : console.log('The world is an odd place!');
-
-// v2: Check for even or odd using If statement
-// if (length % 2 == 0) {
-//   console.log('The world is nice and even!');
-// } else {
-//   console.log('The world is an odd place!');
-// }
-
 
 // Function Declarations
 function sayHello() {
@@ -98,8 +64,6 @@ function sayHello() {
 function checkAge(name, age) {
   if (age < 21) {
     alert('Sorry ' + name + ", you aren't old enough to view this page!");
-    // Using template literal
-    // alert(`Sorry ${name}, you aren't old enough to view this page!`); 
   }
 }
 
